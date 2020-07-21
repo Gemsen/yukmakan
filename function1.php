86075fc50ff6a80c498dcf0b00c1b57defcefc9e
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-07-21 18:27:53 +0700

    Update function1.php

--- function1.php
+++ function1.php
@@ -9,7 +9,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.914".mt_rand(100,999).",115.21".mt_rand(1000,9999)";
+$header[] = "X-Location: -8.675842,115.213179";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
