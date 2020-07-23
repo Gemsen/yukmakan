bee43d2b38ba36e4113e2f26822174941968b7d2
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-07-23 23:22:35 +0700

    Update yuk2.php

--- yuk2.php
+++ yuk2.php
@@ -57,7 +57,7 @@ echo color("nevy"," ===================================\n");
         echo color("white",".");
         sleep(15);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAUGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOJEKAMAN"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","Message: ".$message);
