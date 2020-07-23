fcf065346c6f1a04bce5ca955aa08244a6cffe61
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-07-23 23:27:42 +0700

    Update yuk1.php

--- yuk1.php
+++ yuk1.php
@@ -57,7 +57,7 @@ echo color("nevy"," ===================================\n");
         echo color("yellow",".");
         sleep(15);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOJEKAMAN"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){j
         echo "\n".color("green","+] Message: ".$message);
@@ -85,7 +85,7 @@ echo color("nevy"," ===================================\n");
         echo color("yellow",".");
         sleep(10);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAUGOFOOD2107"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("green","+] Message: ".$message);
         sleep(2);
