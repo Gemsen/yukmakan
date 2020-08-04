f003ad070396227f30bafdd704dd95c6d414e85e
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-08-05 00:51:21 +0700

    Update yuk1.php

--- yuk1.php
+++ yuk1.php
@@ -57,7 +57,7 @@ echo color("nevy"," ===================================\n");
         echo color("yellow",".");
         sleep(15);
         }
-        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAUGOFOOD2107"}');
+        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAUGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
@@ -71,7 +71,7 @@ echo color("nevy"," ===================================\n");
         echo color("yellow",".");
         sleep(15);
         }
-        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$message);
@@ -85,7 +85,7 @@ echo color("nevy"," ===================================\n");
         echo color("yellow",".");
         sleep(10);
         }
-        $code1 = request1('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PENGENGOFOOD2107"}');
+        $code1 = request1('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PENGENGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("green","+] Message: ".$message);
         sleep(2);
